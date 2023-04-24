143a4c0574c57ee2402fc2fb2d758df693d014b5
rasko1 <oophn.88@yandex.ru>
2023-04-24 13:57:51 +0300

    reformat code

--- src/main/java/Radio/Radio.java
+++ src/main/java/Radio/Radio.java
@@ -1,67 +1,77 @@
 package Radio;
 
 public class Radio {
-private int minStation;
-private final int maxStation;
-private int currentFrequency = minStation;
-private int minVolume;
-private int maxVolume;
-public int currentVolume = minVolume;
+    private int minStation;
+    private final int maxStation;
+    private int currentFrequency = minStation;
+    private int minVolume;
+    private int maxVolume;
+    public int currentVolume = minVolume;
 
-public Radio(){
-    this.minStation = 0;
-    this.maxStation = 9;
-    this.minVolume = 0;
-    this.maxVolume = 100;
-}
-public Radio(int countStation) {
-    this.maxStation = countStation - 1;
-}
-public int getCurrentFrequency() {
-    return currentFrequency;
-}
+    public Radio() {
+        this.minStation = 0;
+        this.maxStation = 9;
+        this.minVolume = 0;
+        this.maxVolume = 100;
+    }
+
+    public Radio(int countStation) {
+        this.maxStation = countStation - 1;
+    }
+
+    public int getCurrentFrequency() {
+        return currentFrequency;
+    }
+
     public int getCurrentVolume() {
         return currentVolume;
     }
- public void setCurrentVolume(Integer newCurrentVolume) {
-    if (newCurrentVolume <= minVolume){
-         newCurrentVolume = minVolume;
-     }
-    if (newCurrentVolume >= maxVolume) {
-        newCurrentVolume = maxVolume;
-     }
-    currentVolume = newCurrentVolume;
- }
-public void setDirectAccessToFrequency(int newCurrentFrequency) {
-if (newCurrentFrequency<minStation){
-    newCurrentFrequency = minStation;
-}
 
-
-if (newCurrentFrequency > maxStation) {
-    newCurrentFrequency = maxStation;
-}
-currentFrequency = newCurrentFrequency;
-}
-    public void setNextFrequency() {
-         if (currentFrequency == maxStation) {  currentFrequency = minStation -1;
+    public void setCurrentVolume(Integer newCurrentVolume) {
+        if (newCurrentVolume <= minVolume) {
+            newCurrentVolume = minVolume;
         }
-        currentFrequency = currentFrequency +1;
+        if (newCurrentVolume >= maxVolume) {
+            newCurrentVolume = maxVolume;
+        }
+        currentVolume = newCurrentVolume;
     }
+
+    public void setDirectAccessToFrequency(int newCurrentFrequency) {
+        if (newCurrentFrequency < minStation) {
+            newCurrentFrequency = minStation;
+        }
+
+
+        if (newCurrentFrequency > maxStation) {
+            newCurrentFrequency = maxStation;
+        }
+        currentFrequency = newCurrentFrequency;
+    }
+
+    public void setNextFrequency() {
+        if (currentFrequency == maxStation) {
+            currentFrequency = minStation - 1;
+        }
+        currentFrequency = currentFrequency + 1;
+    }
+
     public void setPrevFrequency() {
         if (currentFrequency == minStation) {
             currentFrequency = maxStation + 1;
         }
         currentFrequency = currentFrequency - 1;
- }
+    }
+
     public void increaseVolume() {
         if (currentVolume > minVolume && currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
         }
     }
+
     public void decreaseVolume() {
         if (currentVolume > minVolume && currentVolume < maxVolume) {
-            currentVolume = currentVolume -1;
+            currentVolume = currentVolume - 1;
         }
     }
 }
